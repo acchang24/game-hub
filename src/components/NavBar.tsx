f121a9f21121490e6e7be3282d254bebc3fa7a75
acchang24 <acchang24@gmail.com>
2023-07-10 18:40:23 -0700

    Created basic layout of the website

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <div>Logo</div>
+      <div>search</div>
+      <div>toggler</div>
+    </nav>
+  );
+};
+
+export default NavBar;
