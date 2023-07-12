5594edfe9db81902670762e32b0e4a9491658bd0
acchang24 <acchang24@gmail.com>
2023-07-12 01:14:40 -0700

    Fetch game titles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react";
+import { CanceledError } from "axios";
+import apiClient from "../services/api-client";
 import "./GameGrid.css";
 
+interface Game {
+  id: number;
+  name: string;
+}
+
 const GameGrid = () => {
-  return <div className="game-grid">GameGrid</div>;
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get("/games", { signal: controller.signal })
+      .then((results) => {
+        console.log(results.data.results);
+        setGames(results.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        else {
+          setError(err.message);
+        }
+      });
+    return () => controller.abort();
+  }, []);
+
+  return (
+    <>
+      {error && <p>{error}</p>}
+      <div className="game-grid">
+        <ul>
+          {games.map((g) => (
+            <li key={g.id}>{g.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default GameGrid;
