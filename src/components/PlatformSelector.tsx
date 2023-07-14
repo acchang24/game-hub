99334a0fe74814f20b8733bb376765b2e9cc6069
acchang24 <acchang24@gmail.com>
2023-07-14 02:21:32 -0700

    Hide dropdown when clicked outside of div

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BsChevronDown } from "react-icons/bs";
 import fetchData from "../utility/fetchData";
 import { Platform } from "./GameGrid";
@@ -10,6 +10,28 @@ const PlatformSelector = () => {
   const { data } = fetchData<Platform>("/platforms/lists/parents");
   const [platformsShown, setPlatformsShown] = useState<boolean>(false);
   const [currentPlatform, setCurrentPlatform] = useState<string>("");
+
+  // Reference dropdown div
+  const ref = useRef<HTMLDivElement>(null);
+
+  // Handles clicks outside of the platforms div and hides it
+  const clickOutside = (ref: any) => {
+    useEffect(() => {
+      function handleClickOutside(event: any) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setPlatformsShown(false);
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      // Unbind the event listener on clean up
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  };
+  // Pass in the ref to the click function
+  clickOutside(ref);
 
   return (
     <>
@@ -27,7 +49,7 @@ const PlatformSelector = () => {
         </div>
       </button>
       {platformsShown && (
-        <div className="platform-dropdown">
+        <div className="platform-dropdown" ref={ref}>
           <ul className="platform-list">
             <li key={0}>
               <button
