c78a8fbb97ccea69a304bb6d98dac47d9dfb881c
acchang24 <acchang24@gmail.com>
2023-07-14 13:21:09 -0700

    Reusable selector component

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,87 +1,19 @@
-import { useEffect, useRef, useState } from "react";
-import { BsChevronDown } from "react-icons/bs";
 import fetchData from "../utility/fetchData";
 import { Platform } from "./GameGrid";
-import "./PlatformSelector.css";
+import Selector from "./Selector";
 
 // Returns a drop down selector to filter game platforms
 const PlatformSelector = () => {
-  // Keep track of platform data, when to show it, and the currently selected platform
+  // Keep track/fetch platform data
   const { data } = fetchData<Platform>("/platforms/lists/parents");
-  const [platformsShown, setPlatformsShown] = useState<boolean>(false);
-  const [currentPlatform, setCurrentPlatform] = useState<string>("");
 
-  // Reference dropdown div
-  const ref = useRef<HTMLDivElement>(null);
+  // Create an array of platforms to pass to the selector
+  const platforms = data.map((platform) => ({
+    id: platform.id,
+    name: platform.name,
+  }));
 
-  // Handles clicks outside of the platforms div and hides it
-  const clickOutside = (ref: any) => {
-    useEffect(() => {
-      function handleClickOutside(event: any) {
-        if (ref.current && !ref.current.contains(event.target)) {
-          setPlatformsShown(false);
-        }
-      }
-      // Bind the event listener
-      document.addEventListener("mousedown", handleClickOutside);
-      // Unbind the event listener on clean up
-      return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }, [ref]);
-  };
-  // Pass in the ref to the click function
-  clickOutside(ref);
-
-  return (
-    <div ref={ref} className="selector">
-      <button
-        className="selector-btn"
-        onClick={() => {
-          setPlatformsShown(!platformsShown);
-        }}
-      >
-        <div className="platform-selector">
-          <div>{currentPlatform === "" ? "Platforms" : currentPlatform}</div>
-          <div className="chevron-icon">
-            <BsChevronDown></BsChevronDown>
-          </div>
-        </div>
-      </button>
-      {platformsShown && (
-        <div className="platform-dropdown">
-          <ul className="platform-list">
-            <li key={0}>
-              <button
-                className="platform-btn"
-                onClick={() => {
-                  setCurrentPlatform("");
-                  setPlatformsShown(false);
-                }}
-              >
-                <div>None</div>
-              </button>
-            </li>
-            {data.map((platform) => {
-              return (
-                <li key={platform.id}>
-                  <button
-                    className="platform-btn"
-                    onClick={() => {
-                      setPlatformsShown(false);
-                      setCurrentPlatform(platform.name);
-                    }}
-                  >
-                    <div>{platform.name}</div>
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+  return <Selector data={platforms} placeHolder="Platforms"></Selector>;
 };
 
 export default PlatformSelector;
---
+++ src/components/Selector.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useRef, useState } from "react";
+import { BsChevronDown } from "react-icons/bs";
+import "./Selector.css";
+
+// Props to pass in list data as well as placeholder info
+interface Props {
+  data: { id: number; name: string }[];
+  placeHolder: string;
+}
+
+// Selector component returns a dropdown list
+const Selector = ({ data, placeHolder }: Props) => {
+  // Keep track of when to show list items, and the currently selected item
+  const [itemsShown, setItemsShown] = useState<boolean>(false);
+  const [selectedItem, setSelectedItem] = useState<string>("");
+
+  // Reference dropdown div
+  const ref = useRef<HTMLDivElement>(null);
+
+  // Handles clicks outside of the platforms div and hides it
+  const clickOutside = (ref: any) => {
+    useEffect(() => {
+      function handleClickOutside(event: any) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setItemsShown(false);
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      // Unbind the event listener on clean up
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  };
+  // Pass in the ref to the click function
+  clickOutside(ref);
+
+  return (
+    <div ref={ref} className="selector">
+      <button
+        className="selector-btn"
+        onClick={() => {
+          setItemsShown(!itemsShown);
+        }}
+      >
+        <div className="item-selector">
+          <div>{selectedItem === "" ? placeHolder : selectedItem}</div>
+          <div className="chevron-icon">
+            <BsChevronDown></BsChevronDown>
+          </div>
+        </div>
+      </button>
+      {itemsShown && (
+        <div className="item-dropdown">
+          <ul className="item-list">
+            <li key={0}>
+              <button
+                className="item-btn"
+                onClick={() => {
+                  setSelectedItem("");
+                  setItemsShown(false);
+                }}
+              >
+                <div>None</div>
+              </button>
+            </li>
+            {data.map((item) => {
+              return (
+                <li key={item.id}>
+                  <button
+                    className="item-btn"
+                    onClick={() => {
+                      setItemsShown(false);
+                      setSelectedItem(item.name);
+                    }}
+                  >
+                    <div>{item.name}</div>
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Selector;
