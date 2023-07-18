7353b76b00096a038424e7c9a8eaff11babafc2d
acchang24 <acchang24@gmail.com>
2023-07-18 13:34:55 -0700

    setup react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Create new instance of queryclient
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools></ReactQueryDevtools>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
