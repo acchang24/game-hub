7236de6fe0fbfb9b2bc831e61d572c2b60b4a118
acchang24 <acchang24@gmail.com>
2023-07-20 02:59:10 -0700

    add trailers

--- src/hooks/useGetGame.ts
+++ src/hooks/useGetGame.ts
@@ -5,7 +5,7 @@ import { Game } from "../interfaces/Game";
 const apiClient = new ApiClient<Game>("/games");
 
 const useGetGame = (slug: string) =>
-  useQuery<Game, Error>({
+  useQuery({
     queryKey: ["game", slug],
     queryFn: () => apiClient.get(slug),
   });
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import useGetGame from "../hooks/useGetGame";
 import ExpandableText from "../components/ExpandableText";
 import DefinitionItem from "../components/DefinitionItem";
+import GameTrailer from "../components/GameTrailer";
 import "./css/GameDetailsPage.css";
 
 const GameDetailPage = () => {
@@ -10,42 +11,41 @@ const GameDetailPage = () => {
   // Get the game based on id/slug name
   const { data: game, error } = useGetGame(id!);
 
+  if (error || !game) return null;
+
   return (
     <>
-      {error ? (
-        <p>{error.message}</p>
-      ) : (
-        <div className="game-detail">
-          <h1>{game?.name}</h1>
-          <ExpandableText>{game?.description_raw}</ExpandableText>
-          <div className="info-grid">
-            <DefinitionItem name="Platforms">
-              {game?.parent_platforms?.map((p) => (
-                <p key={p.platform.id}>{p.platform.name}</p>
-              ))}
-            </DefinitionItem>
-            <DefinitionItem name="Genres">
-              {game?.genres?.map((g) => (
-                <p key={g.id}>{g.name}</p>
-              ))}
-            </DefinitionItem>
-            <DefinitionItem name="ESRB Rating">
-              {game?.esrb_rating?.name || "None"}
-            </DefinitionItem>
-            <DefinitionItem name="Metacritic Rating">
-              {game?.metacritic || "None"}
-            </DefinitionItem>
-            <DefinitionItem name="Release Date">
-              {game?.released || "None"}
-            </DefinitionItem>
-            <DefinitionItem name="Publishers">
-              {game?.publishers?.map((p) => (
-                <p key={p.id}>{p.name}</p>
-              ))}
-            </DefinitionItem>
-          </div>
+      <div className="game-detail">
+        <h1>{game.name}</h1>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <div className="info-grid">
+          <DefinitionItem name="Platforms">
+            {game.parent_platforms.map((p) => (
+              <p key={p.platform.id}>{p.platform.name}</p>
+            ))}
+          </DefinitionItem>
+          <DefinitionItem name="Genres">
+            {game.genres.map((g) => (
+              <p key={g.id}>{g.name}</p>
+            ))}
+          </DefinitionItem>
+          <DefinitionItem name="ESRB Rating">
+            {game.esrb_rating?.name || "None"}
+          </DefinitionItem>
+          <DefinitionItem name="Metacritic Rating">
+            {game.metacritic || "None"}
+          </DefinitionItem>
+          <DefinitionItem name="Release Date">
+            {game.released || "None"}
+          </DefinitionItem>
+          <DefinitionItem name="Publishers">
+            {game.publishers.map((p) => (
+              <p key={p.id}>{p.name}</p>
+            ))}
+          </DefinitionItem>
         </div>
-      )}
+        <GameTrailer gameId={game.id}></GameTrailer>
+      </div>
     </>
   );
 };
